6a98f2337fc59c40ea258b1fadea721ea7ba31f9
Daniel Perrett <perrettdl@googlemail.com>
2012-11-25 19:34:45 +0000

    Added Test::Proto::Base2, one issue still remains with TestRunner registering passes.

---
+++ lib/Test/Proto/Base2.pm
@@ -0,0 +1,89 @@
+package Test::Proto::Base2;
+use 5.006;
+use strict;
+use warnings;
+use Test::Proto::TestRunner;
+
+our $defined_tests = {};
+sub initialise
+{
+	return $_[0];
+}
+
+sub new
+{
+	my $class = shift;
+	my $self = bless {
+		script=>[],
+	}, $class;
+	return $self->initialise;
+}
+
+sub clone
+{
+	my $self = shift;
+	my $new = bless {
+		script=>$self->{'script'},
+	}, ref $self;
+	return $new;
+}
+sub add_test{
+	my $self = shift;
+	push $self->{script}, [@_];
+	return $self;
+}
+
+sub run_test{
+	my ($self, $test, $subject, $context) = @_;
+	print ref $context;
+	print "\n- ".(defined $_ ? $_: '[undefined]') foreach @$test;
+	my $package = ref $self;
+	$defined_tests->{$test->[0]}
+		->($context, $subject, @$test);
+	return $self;
+}
+
+sub defined_tests {
+	return $defined_tests;
+}
+
+sub run_tests{
+	my ($self, $subject, $context) = @_;
+	foreach my $test (@{$self->{script}}){
+		run_test($self, $test, $subject, $context);
+	}
+}
+
+sub define_test{
+	my ($testName, $testSub) = @_;
+	my ($package, $filename, $line) = caller;
+	$defined_tests->{$testName} = $testSub;
+	# return value of this not specified 
+}
+
+sub is {
+	my ($self, $expected, $reason) = @_;
+	$self->add_test('is', $expected, $reason);
+	return $self;
+};
+
+define_test is => sub {
+	my ($self, $subject, $test, $expected, $reason) = @_; # self is the context, NOT the prototype
+	if($subject eq $expected) o{
+		return $self->test_pass($subject, $test, $expected, $reason); 
+	}
+	else {
+		return $self->test_fail($subject, $test, $expected, $reason);
+	}
+};
+
+sub validate
+{
+	my ($self, $subject, $context) = @_;
+	if (!defined $context or !ref $context){ # if context is not a TestRunner
+		$context = Test::Proto::TestRunner->new($context);
+	}
+	run_tests($self, $subject, $context);
+	return $context;
+}
+
--- t/Test-Proto-TestRunner.t
+++ t/Test-Proto-TestRunner.t
@@ -7,10 +7,19 @@ use Test::Proto::TestRunner;
 ok (1, 'ok is ok');
 sub runner { Test::Proto::TestRunner->new(@_) };
 
-ok runner->test_fail()->isa('Test::Proto::Fail');
-ok runner->test_pass()->isa('Test::Proto::Pass');
-ok runner->test_diag()->isa('Test::Proto::Diag');
-ok runner->test_note()->isa('Test::Proto::Diag');
+ok runner->test_fail()->isa('Test::Proto::Fail'), 'runner->test_fail() returns the correct sort of object';
+ok runner->test_pass()->isa('Test::Proto::Pass'), 'runner->test_pass() returns the correct sort of object';
+ok runner->test_diag()->isa('Test::Proto::Diag'), 'runner->test_fail() returns the correct sort of object';
+ok runner->test_note()->isa('Test::Proto::Diag'), 'runner->test_note() returns the correct sort of object';
+
+my $pass_runner = runner;
+$pass_runner->test_pass();
+ok($pass_runner, 'A runner with a pass passes');
+my $fail_runner = runner;
+$fail_runner->test_fail();
+ok(!$fail_runner, 'A runner with a fail fails');
+$fail_runner->test_pass();
+ok(!$fail_runner, 'A runner with a fail then a pass still fails');
 
 # runner->test_location();
 done_testing;
